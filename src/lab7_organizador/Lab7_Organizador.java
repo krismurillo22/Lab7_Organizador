2ca35b0a720657bf0f9e34edcb67b6ceeecb914e
krismurillo22 <kristian_murillo@unitec.edu>
2024-09-06 13:19:27 -0600

    Subida

---
+++ src/lab7_organizador/Lab7_Organizador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab7_organizador;
+
+/**
+ *
+ * @author User
+ */
+public class Lab7_Organizador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
